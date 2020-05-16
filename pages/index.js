08644515824e260b1fb34e89d3bb118c6926c399
carboni09 <caleb.adegbite@hotmail.com>
2020-05-16 11:07:49 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Nav from '../components/nav'
+
+export default function IndexPage() {
+  return (
+    <div className="container w-4/5 md:w-3/5 mx-auto">
+
+      <div className="hero">
+        <h1 className="title">Couldn't find a better gift that's expressive of you and has tech written all over it.</h1>
+        <h1 className="subtitle  mt-8">Hope <a href="#" className="text-purple-600">http://oluwatomis.in</a> would be useful for your personal brand. Happy birthday.</h1>
+        <p className="text-center mt-2">- Caleb</p>
+
+      </div>
+    </div>
+  )
+}
